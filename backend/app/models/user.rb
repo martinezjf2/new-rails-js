e06e64b9e19da65094d2b18ed19fc51d273af056
Jeffrey Martinez <martinezjf2@gmail.com>
2020-08-12 14:50:21 -0400

    added validations to my user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_many :books
+    validates :first_name, presence: { message: "can't be empty" }
+    validates :last_name, presence: { message: "can't be empty" }
 end
