ec717d53507aabe35d2bb1804f37dc3e2dfa5f39
Jeffrey Martinez <martinezjf2@gmail.com>
2020-08-12 14:49:29 -0400

    added validations to my models

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :user
+    validates :title, presence: { message: "can't be empty" }
+    validates :author, presence: { message: "can't be empty" }
 end
